6066837d545e374c1f8b159c88640b0bb8918d8f
shrujana <shrujanareddy7@gmail.com>
2024-01-03 12:02:53 +0530

    correct headings

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -10,13 +10,13 @@ function App() {
       <h3 className="font-bold mt-2">Project: WD301</h3>
       <div className="flex mt-4">
         <div className="w-80 h-auto border rounded-lg p-4 mr-4">
-          <h1 className="font-bold text-xl mb-4">Pending Tasks</h1>
+          <h1 className="font-bold text-xl mb-4">Pending</h1>
           <TaskCard type="p" title="Build website" date="Jan 6" assignee="John" />
           <TaskCard type="p" title="Add blog" date="Jan 10" assignee="Ram" />
           <h3 className="mt-4 w-40 h-6 border mb-4" >New task</h3>
         </div>
         <div className="w-80 h-auto border rounded-lg p-4 ml-4">
-          <h1 className="font-bold text-xl mb-4">Completed Tasks</h1>
+          <h1 className="font-bold text-xl mb-4">Done</h1>
           <TaskCard type="d" title="Design the mockup" date="Dec 31" assignee="Rohit" />
           <TaskCard type="d" title="Get approval" date="Dec 29" assignee="Rahul" />
         </div>

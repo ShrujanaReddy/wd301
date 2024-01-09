2dd3f78bdf73f95949385c3198f8d329f5af992c
shrujana <shrujanareddy7@gmail.com>
2024-01-09 23:43:41 +0530

    corrected the added typescript

--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -3,19 +3,24 @@ import './TaskCard.css';
 
 interface TaskCardProps {
   type: string;
-  dueDate? : string;
-  completedAtDate? : string;
+  dueDate?: string;
+  completedAtDate?: string;
   title: string;
   assigneeName: string;
 }
 
 const TaskCard: React.FC<TaskCardProps> = ({ type, dueDate, completedAtDate, title, assigneeName }) => {
-  const displayDate = type === "d" ? `Completed on: ${completedAtDate}` : `Due on: ${dueDate}`;
+  let displayDate = '';
+  if (type === 'd' && completedAtDate) {
+    displayDate = `Completed on: ${completedAtDate}`;
+  } else if (dueDate) {
+    displayDate = `Due on: ${dueDate}`;
+  }
 
   return (
     <div className='TaskItem'>
       <h2 className="text-xl font-bold">{title}</h2>
-      <p>{displayDate}</p>
+      {displayDate && <p>{displayDate}</p>}
       <p>Assignee: {assigneeName}</p>
     </div>
   );

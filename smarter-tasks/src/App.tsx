faf7de574a2bdcd2a611a35f806c1203276b7982
shrujana <shrujanareddy7@gmail.com>
2024-01-30 22:17:17 +0530

    l5

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,39 +1,55 @@
 import {
   createBrowserRouter,
+  Navigate,
   RouterProvider,
 } from "react-router-dom";
-import HomePage from './pages/HomePage';
-import TaskListPage from './pages/TaskListPage';
-import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import TaskListPage from "./pages/TaskListPage";
 import TaskDetailsPage from "./pages/TaskDetailsPage";
+import Signin from "./pages/Signin";
+import ProtectedRoute from "./ProtectedRoute";
+import Layout from "./components/Layout";
 import './App.css';
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter([
   {
+    path: "/",
+    element: <Navigate to="/signin" replace />,
+  },
+  {
+    path: "/signin",
+    element: <Signin />,
+  },
+  {
     element: (
-      <Layout />
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
     ),
     children: [
       {
-        path: "/",
-        element: (<HomePage />)
+        path: "home",
+        element: <HomePage />,
       },
       {
         path: "tasks",
-        element: (<TaskListPage />)
+        element: <TaskListPage />,
       },
       {
         path: "tasks/:id",
-        element: (<TaskDetailsPage />)
+        element: <TaskDetailsPage />,
       },
-    ]
-  }
+    ],
+  },
+  {
+    path: '*',
+    element: <NotFoundPage />,
+  },
 ]);
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  );
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
---
+++ smarter-tasks/src/pages/NotFoundPage.tsx
@@ -0,0 +1,17 @@
+// NotFoundPage.tsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h2>404 - Not Found</h2>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <Link to="/">
+        <button id="backToHomeButton">Back to Homepage</button>
+      </Link>
+    </div>
+  );
+};
+
+export default NotFoundPage;

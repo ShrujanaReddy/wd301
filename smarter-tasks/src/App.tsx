c2c9d5d53cc97ae714f998c2bd19cf5366df8fd6
shrujana <shrujanareddy7@gmail.com>
2024-02-27 20:07:30 +0530

    level 9

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { Suspense, useContext } from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes"
 import { ThemeContext } from "./context/theme";
@@ -7,17 +7,21 @@ import { MembersProvider } from "./context/members/context";
 import { CommentProvider } from "./context/comment/context";
 
 const App = () => {
-  const { theme } = useContext(ThemeContext)
+  const { theme } = useContext(ThemeContext);
   return (
-  <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
-    <ProjectsProvider>
+    <div
+      className={`h-full w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}
+    >
+      <ProjectsProvider>
         <MembersProvider>
           <CommentProvider>
+            <Suspense fallback={<>Loading...</>}>
             <RouterProvider router={router} />
+          </Suspense>
           </CommentProvider>
         </MembersProvider>
-    </ProjectsProvider>
-  </div>
-)
-}
+      </ProjectsProvider>
+    </div>
+  );
+};
 export default App;
--- smarter-tasks/src/routes/index.tsx
+++ smarter-tasks/src/routes/index.tsx
@@ -2,16 +2,19 @@ import { createBrowserRouter,Navigate } from "react-router-dom";
 
 import AccountLayout from "../layouts/account"
 import ProtectedRoute from "./ProtectedRoutes"
-import Signin from "../pages/signin"
-import Signup from "../pages/signup"
-import Projects from "../pages/projects"
-import Members from "../pages/members" 
-import Logout from "../pages/logout";
-import Notfound from "../pages/Notfound";
+import React from "react";
 import ProjectContainer from "../pages/projects/ProjectContainer";
-import ProjectDetails from "../pages/project_details";
-import NewTask from "../pages/tasks/NewTask";
-import TaskDetailsContainer from "../pages/tasks/TaskDetailsContainer";
+import Notfound from "../pages/Notfound";
+const Signin = React.lazy(() => import("../pages/signin"));
+const Signup = React.lazy(() => import("../pages/signup"));
+const Projects = React.lazy(() => import("../pages/projects"));
+const Members = React.lazy(() => import("../pages/members"));
+const Logout = React.lazy(() => import("../pages/logout"));
+const ProjectDetails = React.lazy(() => import("../pages/project_details"));
+const NewTask = React.lazy(() => import("../pages/tasks/NewTask"));
+const TaskDetailsContainer = React.lazy(
+  () => import("../pages/tasks/TaskDetailsContainer")
+);
 
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/account/projects" replace /> },
@@ -35,6 +38,7 @@ const router = createBrowserRouter([
         <AccountLayout />
       </ProtectedRoute>
     ),
+    ErrorBoundary: () => <>Failed to load the page</>,
     children: [
       { index: true, element: <Navigate to="/account/projects" replace /> },
       {

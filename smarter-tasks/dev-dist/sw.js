4d37fe5aed2b6db6f8cf72be8ab6542e3b1ad08c
shrujana <shrujanareddy7@gmail.com>
2024-02-28 22:59:44 +0530

    level 9 milestone

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lh9upmmop48"
+    "revision": "0.5a6p67tb34g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
